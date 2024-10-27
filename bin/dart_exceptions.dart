13940bc2e27c8a95d1bfbca1546ef0710211a7db
henrique.montenegro <henrique.montenegro@slcagricola.com.br>
2024-10-27 09:32:22 -0300

    03.02 - Lançando Exceções com Throw

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -1,63 +1,18 @@
-// void main() {
-//   // Criando o banco
-//   BankController bankController = BankController();
-
-//   // Adicionando contas
-//   bankController.addAccount(
-//       id: "Ricarth",
-//       account:
-//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
-
-//   bankController.addAccount(
-//       id: "Kako",
-//       account:
-//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
-
-//   // Fazendo transferência
-//   bool result = bankController.makeTransfer(
-//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
-
-//   // Observando resultado
-//   print(result);
-// }
-
+import 'controllers/bank_controller.dart';
+import 'models/account.dart';
 
 void main() {
-  print('Started main');
+  // Criando o banco
+  BankController bankController = BankController();
 
-  functionOne();
+  // Adicionando contas
+  bankController.addAccount(id: "Ricarth", account: Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
 
-  print('Finished main');
-}
+  bankController.addAccount(id: "Kako", account: Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
 
-void functionOne() {
-  print('Started functionOne');
+  // Fazendo transferência
+  bool result = bankController.makeTransfer(idSender: "Kako", idReceiver: "Ricarth", amount: 700);
 
-  try {
-    functionTwo();
-  } catch (exception, stackTrace) {
-    print('\nException:');
-    print(exception);
-
-    print('\nStackTrace:');
-    print(stackTrace);
-
-    rethrow;
-  } finally {
-    print('\nFinally functionOne\n');
-  }
-
-  print('Finished functionOne');
-}
-
-void functionTwo() {
-  print('Started functionTwo');
-
-  for (var i = 0; i <= 5; i++) {
-    print(i);
-
-    double amount = double.parse('Not a number');
-  }
-
-  print('Finished functionTwo');
+  // Observando resultado
+  print(result);
 }
---
+++ bin/helpers/stack_call.dart
@@ -0,0 +1,62 @@
+// void main() {
+//   // Criando o banco
+//   BankController bankController = BankController();
+
+//   // Adicionando contas
+//   bankController.addAccount(
+//       id: "Ricarth",
+//       account:
+//           Account(name: "Ricarth Lima", balance: 400, isAuthenticated: true));
+
+//   bankController.addAccount(
+//       id: "Kako",
+//       account:
+//           Account(name: "Caio Couto", balance: 600, isAuthenticated: true));
+
+//   // Fazendo transferência
+//   bool result = bankController.makeTransfer(
+//       idSender: "Kako", idReceiver: "Ricarth", amount: 700);
+
+//   // Observando resultado
+//   print(result);
+// }
+
+void stackStart() {
+  print('Started main');
+
+  functionOne();
+
+  print('Finished main');
+}
+
+void functionOne() {
+  print('Started functionOne');
+
+  try {
+    functionTwo();
+  } catch (exception, stackTrace) {
+    print('\nException:');
+    print(exception);
+
+    print('\nStackTrace:');
+    print(stackTrace);
+
+    rethrow;
+  } finally {
+    print('\nFinally functionOne\n');
+  }
+
+  print('Finished functionOne');
+}
+
+void functionTwo() {
+  print('Started functionTwo');
+
+  for (var i = 0; i <= 5; i++) {
+    print(i);
+
+    double amount = double.parse('Not a number');
+  }
+
+  print('Finished functionTwo');
+}
