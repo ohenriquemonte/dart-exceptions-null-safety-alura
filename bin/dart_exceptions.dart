c3647b519e81a5787cd62862977a654cf8b69adb
henrique.montenegro <henrique.montenegro@slcagricola.com.br>
2024-11-06 19:52:45 -0300

    03.08 - Sobrescrevendo o toString

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -13,19 +13,23 @@ void main() {
 
   // Fazendo transferência
   try {
-    bool result = bankController.makeTransfer(idSender: "Kako", idReceiver: "Ricarth", amount: 599);
+    bool result = bankController.makeTransfer(idSender: "Kako", idReceiver: "Ricarth", amount: 1000);
 
     // Observando resultado
     if (result) {
       print("Transação efetuada com sucesso!");
     }
   } on SenderIdInvalidException catch (e) {
+    print(e);
     print("O ID '${e.idSender}' do remetente é inválido");
   } on ReceiverIdInvalidException catch (e) {
+    print(e);
     print("O ID '${e.idReceiver}' do destinatário é inválido");
   } on SenderNotAuthenticatedException catch (e) {
+    print(e);
     print("O remetente do ID '${e.idSender}' não está autenticado");
   } on SenderBalanceLowerThanAmountException catch (e) {
+    print(e);
     // print("O remetente do ID '${e.idSender}' não possui saldo suficiente");
     print("O remetente do ID '${e.idSender}' tentou enviar ${e.amount} sendo que sua conta tem ${e.senderBalance}");
   } on Exception {
--- bin/exceptions/bank_controller_exception.dart
+++ bin/exceptions/bank_controller_exception.dart
@@ -1,22 +1,41 @@
 class SenderIdInvalidException implements Exception {
+  static const String report = 'SenderIdInvalidException';
   String idSender;
 
   SenderIdInvalidException({required this.idSender});
+
+  @override
+  String toString() {
+    return '$report\nID SENDER: $idSender\n';
+  }
 }
 
 class ReceiverIdInvalidException implements Exception {
+  static const String report = 'ReceiverIdInvalidException';
   String idReceiver;
 
   ReceiverIdInvalidException({required this.idReceiver});
+
+  @override
+  String toString() {
+    return '$report\nID RECEIVER: $idReceiver\n';
+  }
 }
 
 class SenderNotAuthenticatedException implements Exception {
+  static const String report = 'SenderNotAuthenticatedException';
   String idSender;
 
   SenderNotAuthenticatedException({required this.idSender});
+
+  @override
+  String toString() {
+    return '$report\nID SENDER: $idSender\n';
+  }
 }
 
 class SenderBalanceLowerThanAmountException implements Exception {
+  static const String report = 'SenderBalanceLowerThanAmountException';
   String idSender;
   double senderBalance;
   double amount;
@@ -26,4 +45,9 @@ class SenderBalanceLowerThanAmountException implements Exception {
     required this.senderBalance,
     required this.amount,
   });
+
+  @override
+  String toString() {
+    return '$report\nID SENDER: $idSender\nBALANCE: $senderBalance\nAMOUNT: $amount\n';
+  }
 }
