2ec0c934625a47f82cf9dcdf31309a47c65649f9
henrique.montenegro <henrique.montenegro@slcagricola.com.br>
2024-11-10 18:28:21 -0300

    04.09 - Experimentando propriedades opcionais

--- bin/dart_exceptions.dart
+++ bin/dart_exceptions.dart
@@ -5,30 +5,40 @@ import 'exceptions/bank_controller_exception.dart';
 import 'models/account.dart';
 
 void testingNullSafety() {
-  Account? myAccount;
+  Account? myAccount = Account(name: "Ricarth Lima", balance: 200, isAuthenticated: true);
 
   // Simulando uma comunicação externa
   Random rng = Random();
   int randomNumber = rng.nextInt(10);
 
   if (randomNumber <= 5) {
-    myAccount = Account(name: "Ricarth Lima", balance: 200, isAuthenticated: true);
+    myAccount.createdAt = DateTime.now();
   }
 
   print(myAccount.runtimeType);
-  // print(myAccount.balance);
-  // print(myAccount!.balance);
 
+  // teste forçado que não funciona
+  // print(myAccount.balance);
+
+  // conversão direta: má prática
+  // print(myAccount!.balance);
+  // print(myAccount.createdAt!.day);
+
+  // teste usando if-else para conferir a possibilidade nula
   if (myAccount != null) {
     print(myAccount.balance);
+    if (myAccount.createdAt != null) {
+      print(myAccount.createdAt!.day);
+    }
   } else {
     print("myAccount is null");
   }
 
+  // operador ternário
   print(myAccount != null ? myAccount.balance : "myAccount is null");
 
-  // conversão direta: má prática
-  print(myAccount?.balance);
+  // chamada segura
+  // print(myAccount?.balance);
 
   print('\n');
 }
